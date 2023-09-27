003a0c5d798375268f15bec81810d6755ba0dd22
Oldschoolove <e.g.89@hotmail.de>
2023-09-27 10:32:27 +0200

    first commit

---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDYu-w1F37uKWBbC6Dma_Aaz7cN9p_JjYA",
+  authDomain: "facebook-clone-30c23.firebaseapp.com",
+  projectId: "facebook-clone-30c23",
+  storageBucket: "facebook-clone-30c23.appspot.com",
+  messagingSenderId: "869057658277",
+  appId: "1:869057658277:web:e1c1fe40e0a4baccf83c4c",
+};
